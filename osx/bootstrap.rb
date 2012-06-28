c02f0dffc40ccbcb670f1d4e8f34162e3b419541
Jon Suderman <suderman@gmail.com>
2012-06-28 00:16:29 -0600

    Update master

--- osx/bootstrap.rb
+++ osx/bootstrap.rb
@@ -7,7 +7,6 @@ git = "https://dl.dropbox.com/s/y80vt8720xrv5h0/git.pkg"
 # Install git
 system "curl #{git} -o /tmp/git.pkg"
 system "sudo installer -pkg /tmp/git.pkg -target /"
-system "source /etc/profile"
 
 # Install dotfiles
 system "/usr/local/git/bin/git clone https://suderman@github.com/suderman/dotfiles.git ~/.dotfiles"
@@ -15,4 +14,4 @@ system "cd ~/.dotfiles && ./install.rb"
 system "source ~/.zshrc"
 
 # OS X settings, library syncing and apps
-system "~/.osx/install.rb"
+system "cd ~/.osx && ./install.rb"
