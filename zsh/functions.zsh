696196aeb3e683db67e79b6e2f0dc7626b9ddb21
Jonathan Suderman <suderman@gmail.com>
2014-03-25 16:45:21 -0600

    Latest apps

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -9,6 +9,15 @@ if [[ "$platform" == 'Darwin' ]]; then
 
 fi
 
+# v() {
+#   if [ $# == 0 ]; then
+#     mvim
+#   else
+#     mvim --servername $(basename $(pwd)) \
+#          --remote-tab-silent "$@" 1>/dev/null 2>&1
+#   fi
+# }
+
 #Automatically do an ls after each cd
 cd() {
   if [ -n "$1" ]; then
